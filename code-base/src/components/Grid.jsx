b9f4dd3d95c4ad7925c01a56b6a6627020274c82
Collen Maphike <Mavetane>
2021-08-24 19:21:32 +0200

    added grid

--- code-base/src/components/Grid.jsx
+++ code-base/src/components/Grid.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+const numberOfRows = 50;
+const numberOfColumns = 50
 function Grid() {
+    const [grid, setGrid] = useState(() => {
+        const rows = []
+        for (let i = 0; i < numberOfRows; i++) {
+            rows.push(Array.from(Array(numberOfColumns), () => 0))
+        }
+        return rows
+    })
     return (
-        <div>
-            Display
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: `repeat(${numberOfColumns}, 20px)`,
+            textAlign: 'center'
+        }}>
+            {grid.map((rows, rowIndex) => rows.map((column, columnindex) => <div
+                key={`${rowIndex}-${columnindex}`}
+                style={{
+                    width: 20, height: 20,
+                    backgroundColor: grid[rowIndex][columnindex] ? 'black' : undefined,
+                    border: '1px solid black'
+                }} />))}
         </div>
     )
 }
